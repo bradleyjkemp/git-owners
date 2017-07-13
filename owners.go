b7b72e1573fd3d810de2bd2a30289959c6b6d4b6
bradleyjkemp <bradleyjkemp@users.noreply.github.com>
2017-07-13 23:27:37 +0100

    MVP (#1)

---
+++ owners.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	baseBranch := flag.String("base-branch", "master", "Base branch to compare commits against (default master)")
+	flag.Parse()
+
+	baseCommit, err := findBaseCommit(*baseBranch)
+	if err != nil {
+		fmt.Errorf("%s", err)
+	}
+
+	changedFiles, err := findChangedFiles(baseCommit)
+	if err != nil {
+		fmt.Errorf("%s", err)
+	}
+
+	fileToOwners, err := mapFilesToOwners(changedFiles)
+	if err != nil {
+		fmt.Errorf("%s", err)
+	}
+
+	suggestedReviewers := suggestReviewers(fileToOwners)
+
+	fmt.Println(strings.Join(suggestedReviewers, "\n"))
+}
