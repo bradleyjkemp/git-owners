49776cb2b2c00522c6d45567a8b5c58df49b9a1e
bradleyjkemp <scytheon3@gmail.com>
2017-07-20 21:19:09 +0000

    Fix ignore flag, add some cli tests

--- resolver/resolver_test.go
+++ resolver/resolver_test.go
@@ -5,21 +5,30 @@ import (
 	"testing"
 )
 
-var resolverAllOwners = []struct {
+var resolveOwnersTests = []struct {
 	filepath       string
 	expected       []string
 	expectedDirect []string
 }{
-	{"../example_repo/go/src/test/example.go", []string{"bob", "carol", "alice"}, []string{"bob", "carol"}},
-	{"../example_repo/go/src/test/README", []string{"alice"}, []string{"alice"}},
+	{"../example_repo/go/src/test/example.go",
+		[]string{"bob", "carol", "alice", "bradleyjkemp"}, []string{"bob", "carol"}},
+
+	// should only own files where the pattern matches
+	{"../example_repo/go/src/test/README",
+		[]string{"carol", "alice", "bradleyjkemp"}, []string{"carol"}},
+
+	// ignored files should always be ignored even if there is an owner lower down
 	{"../example_repo/go/BUILD", nil, nil},
 	{"../example_repo/BUILD", nil, nil},
 	{"../example_repo/js/BUILD", nil, nil},
-	{"../example_repo/js/static/test.png", []string{"dave@example.com"}, []string{"dave@example.com"}},
+
+	// should respect the @set noparent flag
+	{"../example_repo/js/static/test.png",
+		[]string{"dave@example.com"}, []string{"dave@example.com"}},
 }
 
 func TestResolverAllOwners(t *testing.T) {
-	for _, testCase := range resolverAllOwners {
+	for _, testCase := range resolveOwnersTests {
 		actual, err := ResolveOwners(testCase.filepath, true)
 		assert.NoError(t, err, "must not fail to resolve")
 		assert.Equal(t, testCase.expected, actual, testCase.filepath)
@@ -27,7 +36,7 @@ func TestResolverAllOwners(t *testing.T) {
 }
 
 func TestResolverDirectOwners(t *testing.T) {
-	for _, testCase := range resolverAllOwners {
+	for _, testCase := range resolveOwnersTests {
 		actual, err := ResolveOwners(testCase.filepath, false)
 		assert.NoError(t, err, "must not fail to resolve")
 		assert.Equal(t, testCase.expectedDirect, actual, testCase.filepath)
